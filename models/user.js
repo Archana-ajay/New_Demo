03e71148230064087bd2cc9c736d90bbedf648e1
archana <archanaajay45@gmail.com>
2022-12-09 10:04:37 +0530

    test

--- models/user.js
+++ models/user.js
@@ -3,9 +3,9 @@ const { Model } = require("sequelize");
 const hi=require("../utils/pagination");
 const { get } = require("../utils/sendmail");
 const bucket=require('../utils/S3helper')
-const moment = require('moment');
+                          const moment = require('moment');
                                 const a="hi"
-                              const b="hello"
+                               const b="hello"
 
 	
 
